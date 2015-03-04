d101983e7921be790ab3c7b6bf2cae3c8f1ca6cf
Mark Jordan <nyctef@nyctef.com>
2015-03-04 22:33:55 +0000

    fix this implementation

--- OwinLoggingDashboard/InMemoryTraceListener.cs
+++ OwinLoggingDashboard/InMemoryTraceListener.cs
@@ -10,18 +10,18 @@ namespace OwinLoggingDashboard
     class InMemoryTraceListener : TraceListener
     {
         private readonly List<string> m_Messages = new List<string>();
+        private string m_CurrentMessage = "";
 
         public IEnumerable<string> Messages { get { return m_Messages; } }
 
         public override void Write(string message)
         {
-            if (!m_Messages.Any()) m_Messages.Add("");
-            m_Messages[m_Messages.Count - 1] += message;
+            m_CurrentMessage += message;
         }
 
         public override void WriteLine(string message)
         {
-            m_Messages.Add(message);
+            m_Messages.Add(m_CurrentMessage + message);
         }
     }
 }
